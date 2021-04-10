78a411ca0a55d1c7ef1e6b701db734f93e1c34ea
manas-sahu <manas.sahu.one@gmail.com>
2021-04-10 15:54:19 +0530

    hour bug fix

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ let day = new Date();
 let hour = day.getHours() * 30;
 let min = day.getMinutes() * deg;
 let sec = day.getSeconds() * deg;
-hr.style.transform = `rotatez(${hour - (min/12)}deg)`;
+hr.style.transform = `rotatez(${hour + (min/12)}deg)`;
 mn.style.transform = `rotatez(${min}deg)`;
 sc.style.transform = `rotatez(${sec}deg)`;
 }, 1000);
