2a0cf9b171768a3382d10ab99c60ade8d6e2c91c
manas-sahu <manas.sahu.one@gmail.com>
2021-04-10 16:47:47 +0530

    theme toggle added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,32 @@
 const deg = 6;
-const hr = document.querySelector('#hr');
-const mn = document.querySelector('#mn');
-const sc = document.querySelector('#sc');
-setInterval (() => {
-let day = new Date();
-let hour = day.getHours() * 30;
-let min = day.getMinutes() * deg;
-let sec = day.getSeconds() * deg;
-hr.style.transform = `rotatez(${hour + (min/12)}deg)`;
-mn.style.transform = `rotatez(${min}deg)`;
-sc.style.transform = `rotatez(${sec}deg)`;
+
+let body = document.querySelector("#main");
+
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+const sc = document.querySelector("#sc");
+
+let toggle = false;
+
+setInterval(() => {
+  let day = new Date();
+  let hour = day.getHours() * 30;
+  let min = day.getMinutes() * deg;
+  let sec = day.getSeconds() * deg;
+  hr.style.transform = `rotatez(${hour + min / 12}deg)`;
+  mn.style.transform = `rotatez(${min}deg)`;
+  sc.style.transform = `rotatez(${sec}deg)`;
 }, 1000);
+
+function toggleTheme () {
+    console.log("Toggle called");
+    toggle = !toggle;
+    if (toggle) {
+        body.classList.add("light");
+    }
+    else {
+        body.classList.remove("light");
+    }
+    
+    
+}
